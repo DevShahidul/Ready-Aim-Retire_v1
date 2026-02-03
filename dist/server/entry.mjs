e644435ff7bea9ebf6f7bb1eef58088a7b163bdd
Shahidul Islam <ishahin56@gmail.com>
2026-02-04 01:18:22 +0600

    Update: added netlify toml file

--- dist/server/entry.mjs
+++ dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_C_OPwgY-.mjs';
-import { manifest } from './manifest_DDWMnaJr.mjs';
+import { manifest } from './manifest_BaM9HhGi.mjs';
 
 const serverIslandMap = new Map();;
 
